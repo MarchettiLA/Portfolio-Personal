af79630e8e316f23fcc1e6f33aabfa90147a417b
Lautaro Alejandro Marchetti <lautaroamarchetti@gmail.com>
2023-08-28 22:22:24 -0300

    Modificaciones basicas nombre y demas solo html
    Modificaciones basicas nombre y demas solo html

--- script/writing-profession.js
+++ script/writing-profession.js
@@ -1,6 +1,6 @@
 
 const typed = new Typed(".typed",{
-    strings : ["Desarrollador Web", "Diseñador Gráfico", "Fotográfo"],
+    strings : ["Diseñador Gráfico", "Fotográfo", "Editor de video"],
     startDelay: 1200,
     typeSpeed: 50,
     backSpeed: 50,
